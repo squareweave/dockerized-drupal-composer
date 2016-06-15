dbcb753bbadd3191365878461ee7c8c06895be97
Sam Jarrett <sam.jarrett@squareweave.com.au>
2016-06-15 13:34:48 +1000

    Move DB settings into settings.php and out of settings.local.php

--- 8/sites/default/settings.php
+++ 8/sites/default/settings.php
@@ -718,6 +718,17 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 #   include __DIR__ . '/settings.local.php';
 # }
 
+$databases['default']['default'] = array(
+    'driver' => 'mysql',
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'host' => getenv('DB_HOST'),
+    'port' => getenv('DB_PORT') ?: 3306,
+    'prefix' => getenv('DB_PREFIX') ?: 'drupal_',
+    'collation' => getenv('DB_COLLATE') ?: 'utf8mb4_general_ci',
+);
+
 if (file_exists(__DIR__ . '/settings.local.php')) {
     include __DIR__ . '/settings.local.php';
 }
