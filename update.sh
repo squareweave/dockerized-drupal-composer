7c43a8f8e7750d4716887125bc822fcbd219b067
Henry Singleton <henry.singleton@squareweave.com.au>
2017-10-25 13:37:17 +1100

    Temporarily disable other versions and update PHP version

--- update.sh
+++ update.sh
@@ -23,10 +23,10 @@ for version in "${versions[@]}"; do
 
         cat templates/Dockerfile > $DIRECTORY/Dockerfile
 
-        target="php:7.0-apache"
+        target="php:7.1-apache"
         if [[ $variant =~ fpm.* ]]
         then
-            target="php:7.0-fpm"
+            target="php:7.1-fpm"
         else
             cp templates/apache2*.conf $DIRECTORY/
             {
