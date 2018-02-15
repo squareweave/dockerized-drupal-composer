e0bf0b12cab79166dda68bc8053518b1f27d8e64
Henry Singleton <henry.singleton@squareweave.com.au>
2018-02-15 12:43:58 +1100

    Update based on commit a407401 in branch template

--- 8.4/app/scripts/at-boot.sh
+++ 8.4/app/scripts/at-boot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+if [ -n $MEMORY_LIMIT ]
+then
+    echo "Setting PHP memory limit to ${MEMORY_LIMIT}"
+    echo "memory_limit = ${MEMORY_LIMIT}" > /usr/local/etc/php/conf.d/resources.ini
+fi
+
 : ${DEVELOPMENT_MODE:-false}
 
 if [ "$DEVELOPMENT_MODE" == "true" ]
@@ -21,12 +27,6 @@ then
     /usr/bin/newrelic-install install
     sed -i "s/newrelic.appname = \"PHP Application\"/newrelic.appname = \"${NR_APP_NAME}\"/" \
         /usr/local/etc/php/conf.d/newrelic.ini
-fi
-
-if [ -n $MEMORY_LIMIT ]
-then
-    echo "Setting PHP memory limit to ${MEMORY_LIMIT}"
-    echo "memory_limit = ${MEMORY_LIMIT}" > /usr/local/etc/php/conf.d/resources.ini
 fi
 
 # Wait for the DB to settle
--- 8.4/app/scripts/at-deploy.sh
+++ 8.4/app/scripts/at-deploy.sh
@@ -3,7 +3,7 @@
 dockerize -wait tcp://${DB_HOST:-db}:${DB_PORT:-3306} -timeout 60s
 
 echo "Entering maintenance mode..."
-/usr/local/bin/drupal --root=/app/web  site:maintenance on --quiet
+/usr/local/bin/drush sset system.maintenance_mode TRUE
 echo "Done."
 
 make --directory=/app config-import
