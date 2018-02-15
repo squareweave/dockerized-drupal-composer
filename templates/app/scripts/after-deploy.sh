a407401ee54ecd4788661fb972125bed33c57c2b
Henry Singleton <henry.singleton@squareweave.com.au>
2018-02-15 12:43:26 +1100

    Use drush for maintenance mode.

--- templates/app/scripts/after-deploy.sh
+++ templates/app/scripts/after-deploy.sh
@@ -3,5 +3,5 @@
 dockerize -wait tcp://${DB_HOST:-db}:${DB_PORT:-3306} -timeout 60s
 
 echo "Exiting maintenance mode..."
-/usr/local/bin/drupal --root=/app/web site:maintenance off --quiet
+/usr/local/bin/drush sset system.maintenance_mode FALSE
 echo "Done."
